cf5f5b8e68faa3e0e2209f1fb949df29e50044df
GuluzadehSenan <guluzadehsenan@gmail.com>
2023-05-23 20:52:08 +0400

    hundred_and_decimal fayli gonderildi

---
+++ hundred and decimal.py
@@ -0,0 +1,25 @@
+# git init
+# git add ele sonra onnan geri qayit
+# git commit ele sonra onnan geri qayit
+
+# 3reqemli ededi(384) print ederek 3u yuzluk kimi, 8i onluq kimi, 4u teklik kimi goster
+
+e = 384
+a = 3
+b = 8
+c = 4
+a *= 100
+b *= 10
+c *= 1
+e = a, b, c
+print(e)
+
+d = 678
+a = 6
+b = 7
+c = 8
+a *= 100
+b *= 10
+c *= 1
+d = a, b, c
+print(d)
